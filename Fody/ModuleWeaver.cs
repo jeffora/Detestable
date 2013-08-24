02ae3bd831f051789265bc14de379da19206130c
Jeff Burn <jeff@jeffburn.com>
2013-08-25 00:09:33 +1000

    Add Mono.Cecil and ModuleWeaver

---
+++ Fody/ModuleWeaver.cs
@@ -0,0 +1,16 @@
+﻿using Mono.Cecil;
+
+namespace Detestable.Fody
+{
+    public class ModuleWeaver
+    {
+        public ModuleDefinition ModuleDefinition { get; set; }
+
+        public void Execute()
+        {
+
+ 
+        } 
+
+    }
+}
