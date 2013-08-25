6d98b1cef5a7f63f35eefb5f41e2450bc9ecf1aa
Jeff Burn <jeff@jeffburn.com>
2013-08-25 23:13:25 +1000

    Add basic project structure

--- Fody/ModuleWeaver.cs
+++ Fody/ModuleWeaver.cs
@@ -1,16 +1,23 @@
-﻿using Mono.Cecil;
+﻿using System;
+using Mono.Cecil;
 
 namespace Detestable.Fody
 {
     public class ModuleWeaver
     {
+        public Action<string> LogInfo { get; set; }
+        public Action<string> LogWarning { get; set; }
         public ModuleDefinition ModuleDefinition { get; set; }
+
+        public ModuleWeaver()
+        {
+            LogInfo = _ => { };
+            LogWarning = _ => { };
+        }
 
         public void Execute()
         {
-
  
         } 
-
     }
 }
---
+++ ReferenceAssembly/DetestAttribute.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Detestable
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = false, Inherited = false)]
+    public class DetestAttribute : Attribute
+    {
+    }
+}
