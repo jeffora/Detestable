2a4e46b54b4e6d82a76e031c478344cf9878ae3e
Jeff Burn <jeff@jeffburn.com>
2013-08-26 22:20:25 +1000

    Create Test and AssemblyToProcess projects

---
+++ Tests/AssemblyTypesTests.cs
@@ -0,0 +1,44 @@
+﻿using System.IO;
+using System.Linq;
+using System.Reflection;
+using Detestable.Fody;
+using Mono.Cecil;
+using NUnit.Framework;
+
+namespace Tests
+{
+    [TestFixture]
+    public class AssemblyTypesTests
+    {
+        Assembly _assembly;
+
+        public AssemblyTypesTests()
+        {
+#if DEBUG
+            var configuration = "Debug";
+#else
+            var configuration = "Release";
+#endif
+            var beforeAssemblyPath =
+                Path.GetFullPath(string.Format(@"..\..\..\AssemblyToProcess\bin\{0}\AssemblyToProcess.dll",
+                                               configuration));
+            var afterAssemblyPath = beforeAssemblyPath.Replace(".dll", "AssemblyTypesTests.dll");
+
+            File.Copy(beforeAssemblyPath, afterAssemblyPath, true);
+            var moduleDefinition = ModuleDefinition.ReadModule(afterAssemblyPath);
+
+            var weavingTask = new ModuleWeaver();
+
+            weavingTask.Execute();
+            moduleDefinition.Write(afterAssemblyPath);
+
+            _assembly = Assembly.LoadFile(afterAssemblyPath);
+        }
+
+        [Test]
+        public void WillBeRemovedTypeExists()
+        {
+            Assert.That(_assembly.GetTypes().Any(t => t.Name == "WillBeRemoved" && t.IsClass));
+        }
+    }
+}
